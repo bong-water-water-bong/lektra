c0bfc1e005f3549b1990073caad4de5e973e733c
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2026-05-14 01:33:33 +0200

    Fix dependency naming

--- build_deb.sh
+++ build_deb.sh
@@ -65,7 +65,7 @@ Maintainer: Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
 Homepage: https://codeberg.org/lektra/lektra
 Installed-Size: ${INSTALLED_SIZE:-0}
 Build-Depends: build-essential pkgconf cmake ninja-build g++
-Depends: qt6-base-dev, qt6-tools-dev, qt6-l10n-tools, unzip, zlib1g-dev, libgl1-mesa-dri, mesa-common-dev, qt6-imageformats-plugins, libqt6svg6${LUA_DEP}
+Depends: qt6-base-dev, qt6-tools-dev, qt6-l10n-tools, unzip, zlib1g-dev, libgl1-mesa-dri, mesa-common-dev, qt6-image-formats-plugins, libqt6svg6${LUA_DEP}
 Suggests: qt6-style-kvantum, libdjvulibre-dev
 Description: High performance Document and Image viewer
 EOF
